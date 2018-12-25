0421fa7c7b2bcfce0455810b4f0153588b45c768
mail@serrynaimo.com <tgorissen@Thomass-MacBook-Pro.local>
2018-12-25 14:10:35 +0900

    adding gitignore

--- node_modules/human-readable-ids/src/tpl.js
+++
@@ -1,7 +0,0 @@
-/*jshint -W054 */
-;(function (exports) {
-  'use strict';
-
-  exports.humanReadableIds = exports.humanReadableIds || {};
-  exports.humanReadableIds.{{setname}} = {{set}};
-}('undefined' !== typeof exports && exports || new Function('return this')()));
